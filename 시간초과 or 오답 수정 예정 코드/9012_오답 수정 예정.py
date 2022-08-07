fce56944ab145601f2959dd9cc98b5fbeb65c7b0
luminaries1 <45480263+luminaries1@users.noreply.github.com>
2022-08-08 02:08:42 +0900

    Update 9012_오답 수정 예정.py

--- 시간초과 or 오답 수정 예정 코드/9012_오답 수정 예정.py
+++ 시간초과 or 오답 수정 예정 코드/9012_오답 수정 예정.py
@@ -5,11 +5,11 @@ for i in range(N):
     lst += [a]
 for st in lst:
     b = st.count('(')
-    c = st.count(')')
+    c = st.count(')')                       # 개수가 동일 한것 외에도 체크가 필요할 것 같습니다.
     if b == c:
         if st[0] == '(':
-            if st[-1] == ')':
-                print('YES')
+            if st[-1] == ')':               
+                print('YES')                # 반례로 '() )( ()' 가 있을 것 같네요.
             else:
                 print('NO')
         else:
